a1fc4b3fcb296fa3b958dd7dad58cb562eb77cac
Matt Delanuez <matt.delanuez@gmail.com>
2024-08-21 20:47:09 -0400

    CSS Solid, Working on API implementation
    starting of weather app. Css is solid but needs the data from the api to drive it. Working on getting it figured out. Currently have issue with Current_weather or another error.

---
+++ weather.js
@@ -0,0 +1,52 @@
+import axios from 'axios';
+
+
+export function getWeather(lat,lon,timezone){
+    return axios.get("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,weather_code,wind_speed_10m&hourly=temperature_2m,apparent_temperature,precipitation_probability,weather_code,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&timeformat=unixtime&timezone=America%2FNew_York", 
+        {
+        params: {
+                latitude: lat,
+                longitude: lon,  
+                timezone: timezone,
+            },
+        }
+     ).then(({data}) => {
+         return {
+                current:parseCurrentWeather(data),
+    // //         daily:parseDailyWeather(data),
+    // //         hourly:parseHourlyWeather(data),
+            }
+        })
+   
+
+}
+
+
+function parseCurrentWeather({current_weather, daily}){
+    const{
+        temperature: currentTemp,
+        windspeed: windSpeed,
+        weathercode: iconCode,
+    } = current_weather
+    
+
+    const{
+        temperature_2m_max: [maxTemp],
+        temperature_2m_min: [minTemp],
+        apparent_temperature_max: [maxFeelsLike],
+        apparent_temperature_min: [minFeelsLike],
+        precipitation_sum: [precip],
+    } = daily
+    
+
+    return {
+        currentTemp: Math.round(currentTemp),
+        highTemp:Math.round(maxTemp),
+        lowTemp:Math.round(minTemp),
+        highFeelsLike:Math.round(maxFeelsLike),
+        lowFeelsLike:Math.round(minFeelsLike),
+        precip: Math.round(precip * 100) / 100,
+        windSpeed: Math.round(windSpeed),
+        iconCode,
+    }
+}
