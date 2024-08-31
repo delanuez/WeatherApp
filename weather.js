aecf3d6189e233cfdb8c1d414b1f2ece9be4ae50
Matt Delanuez <matt.delanuez@gmail.com>
2024-08-31 11:21:14 -0400

    fixed API
    URL for api was breaking the code. New UR fixes this

--- weather.js
+++ weather.js
@@ -2,35 +2,44 @@ import axios from 'axios';
 
 
 export function getWeather(lat,lon,timezone){
-    return axios.get("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,weather_code,wind_speed_10m&hourly=temperature_2m,apparent_temperature,precipitation_probability,weather_code,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&timeformat=unixtime&timezone=America%2FNew_York", 
-        {
-        params: {
-                latitude: lat,
-                longitude: lon,  
-                timezone: timezone,
-            },
+    return axios
+        .get("https://api.open-meteo.com/v1/forecast?hourly=temperature_2m,apparent_temperature,precipitation,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timeformat=unixtime", 
+            {
+                params: {
+                    latitude: lat,
+                    longitude: lon,  
+                    timezone,
+                },
+            }
+        )
+        .then(({data}) => {
+            if (!data || !data.current_weather || !data.daily) {
+            throw new Error("Incomplete weather data");
         }
-     ).then(({data}) => {
-         return {
-                current:parseCurrentWeather(data),
-    // //         daily:parseDailyWeather(data),
-    // //         hourly:parseHourlyWeather(data),
+            return {
+                current: parseCurrentWeather(data),
+    // //       daily:parseDailyWeather(data),
+    // //       hourly:parseHourlyWeather(data),
             }
         })
    
 
 }
 
+console.log();
 
-function parseCurrentWeather({current_weather, daily}){
-    const{
+function parseCurrentWeather({ current_weather, daily}){
+    if (!current_weather || !daily) {
+        throw new Error("Incomplete weather data");
+    }
+    const {
         temperature: currentTemp,
         windspeed: windSpeed,
         weathercode: iconCode,
     } = current_weather
     
 
-    const{
+    const {
         temperature_2m_max: [maxTemp],
         temperature_2m_min: [minTemp],
         apparent_temperature_max: [maxFeelsLike],
